5e200ea6fcd0bbab9fdc694a4258dc69f9053ed2
GabrielMato <58472077+GabrielMatoBoopetyScoopWoop@users.noreply.github.com>
2020-11-16 08:55:57 +0100

    Add files via upload

---
+++ tryCatch01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryCatch01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int broj;
+                Console.Write("Unesite broj = ");
+                broj = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Uneseni broj = " + broj);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
